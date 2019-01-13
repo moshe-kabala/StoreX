3beb57208e162d37a90efe85c979f6a0be3644d0
johnAtt <jonathan.attali23@gmail.com>
2019-01-13 16:49:06 +0200

    add csv test

--- packages/container/src/transform/csv.trans.ts
+++ packages/container/src/transform/csv.trans.ts
@@ -4,26 +4,31 @@ const rowDelim = "\n";
 const colDelim = '","';
 
 /**
- * Converts an array of objects into a CSV table.
- * @param {Array} objArray An array of objects.
- * @param {Array} config An array of the fields the table should have
- */
+* Converts an array of objects into a CSV table.
+* @param {Array} objArray An array of objects.
+* @param {Array} config An array of the fields the table should have
+*/
 export function csvTransform(objArray, schema) {
   let headings = [];
   for (const key in schema.properties) {
     const val = schema.properties[key];
     headings.push(val.title || key);
   }
-
+  
   let output = createRow(headings);
-
+  
   for (var i = 0; i < objArray.length; i++) {
     let element = objArray.data ? objArray.data[i] : objArray[i];
     let obj = element;
     let row = [];
     let val;
     for (const key in obj) {
-      val = obj[key];
+      if ( typeof key === 'object')
+      {
+      Object.keys(key).map(p => val.push(p))
+      } else {
+        val = obj[key];
+      }
       row.push(val || "");
     }
     output += rowDelim + createRow(row);
@@ -36,7 +41,7 @@ function getParamByPath(path, obj) {
   var type = obj.type;
   var key = arrayPath[0];
   var obj = obj;
-
+  
   for (var i = 1; i < arrayPath.length; i++) {
     if (!obj.hasOwnProperty(key)) return -1; // ("the path: "+path+"  is not correct on the obj.type:" +type);
     obj = obj[key];
@@ -52,19 +57,19 @@ function getParamByPath(path, obj) {
 function convertToType(val, columnMetaData) {
   switch (columnMetaData.type) {
     case "date":
-      return new CustomDate().update(val).asCsv();
+    return new CustomDate().update(val).asCsv();
     case "enum":
-      if (columnMetaData.options instanceof Array)
-        // if exist options take the name of the enum
-        columnMetaData.options.some(function (item) {
-          if (item.value == val) {
-            val == item.name;
-            return true;
-          }
-        });
-      return val;
+    if (columnMetaData.options instanceof Array)
+    // if exist options take the name of the enum
+    columnMetaData.options.some(function (item) {
+      if (item.value == val) {
+        val == item.name;
+        return true;
+      }
+    });
+    return val;
     default:
-      return val;
+    return val;
   }
 }
 
---
+++ packages/container/test/csv.test.ts
@@ -0,0 +1,31 @@
+import "jest";
+import {csvTransform}  from "../../container/src/transform/csv.trans";
+
+describe("Csv", () => {
+  
+  test("only string table", async () => {
+    const res = await csvTransform([{ name:"Bob" }], {properties:{name:{title:"Name"}}} );
+    expect(res).toEqual(`"Name"\n"Bob"`)
+  });
+  
+  
+  test("CyberAttackTable", async () => {
+    const res = await csvTransform({data:[{ name:"Bob" }], length: 1}, {properties:{name:{title:"Name"}}} );
+    expect(res).toEqual(`"Name"\n"Bob"`)
+  });
+  
+  test("PolicyMonitorTable", async () => {
+    const res = await csvTransform({data:data, length: 1}, {properties:{meta: {title: "Details"}}} );
+    expect(res).toEqual(`"Details"\n"modification_time:1547127985.394509, modifier: "System", namespace : "from-traffic""`)
+  });
+  
+});
+
+const data = 
+[
+  {
+    meta : { modification_time:1547127985.394509, modifier: "System", namespace : "from-traffic"},
+  }
+]
+  
+
