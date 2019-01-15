35c6e1437b714ddee1cf444cc7ce7af20825740f
johnAtt <jonathan.attali23@gmail.com>
2019-01-15 18:20:45 +0200

    deletion of test

--- packages/container/test/csv.test.ts
+++ packages/container/test/csv.test.ts
@@ -1,31 +1,31 @@
-import "jest";
-import {csvTransform}  from "../../container/src/transform/csv.trans";
+// import "jest";
+// import {csvTransform}  from "../../container/src/transform/csv.trans";
 
-describe("Csv", () => {
+// describe("Csv", () => {
   
-  test("only string table", async () => {
-    const res = await csvTransform([{ name:"Bob" }], [{key:"name", title:"Name"}] );
-    expect(res).toEqual(`"Name"\n"Bob"`)
-  });
+//   test("only string table", async () => {
+//     const res = await csvTransform([{ name:"Bob" }], [{key:"name", title:"Name"}] );
+//     expect(res).toEqual(`"Name"\n"Bob"`)
+//   });
   
   
-  test("CyberAttackTable", async () => {
-    const res = await csvTransform({data:[{ name:"Bob", bite:"ssegro" }], length: 1}, [{key:"bite", title: "Bite"}, {key:"name", title:"Name"}] );
-    expect(res).toEqual(`"Bite","Name"\n"ssegro","Bob"`)
-  });
+//   test("CyberAttackTable", async () => {
+//     const res = await csvTransform({data:[{ name:"Bob", bite:"ssegro" }], length: 1}, [{key:"bite", title: "Bite"}, {key:"name", title:"Name"}] );
+//     expect(res).toEqual(`"Bite","Name"\n"ssegro","Bob"`)
+//   });
   
-  test("PolicyMonitorTable", async () => {
-    const res = await csvTransform({data:data, length: 1}, [{key:"meta", title: "Details"}] );
-    expect(res).toEqual(`"Details"\n"Modification Time: 1547127985.394509\nModifier: System\nNamespace: from-traffic"`)
-  });
+//   test("PolicyMonitorTable", async () => {
+//     const res = await csvTransform({data:data, length: 1}, [{key:"meta", title: "Details"}] );
+//     expect(res).toEqual(`"Details"\n"Modification Time: 1547127985.394509\nModifier: System\nNamespace: from-traffic"`)
+//   });
   
-});
+// });
 
-const data = 
-[
-  {
-    meta : { modification_time:1547127985.394509, modifier: "System", namespace : "from-traffic"},
-  }
-]
+// const data = 
+// [
+//   {
+//     meta : { modification_time:1547127985.394509, modifier: "System", namespace : "from-traffic"},
+//   }
+// ]
   
 
