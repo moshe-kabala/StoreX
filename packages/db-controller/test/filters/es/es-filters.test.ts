ab2c903f169eaed6930c0ae2043590440b03b44e
barlesh <barlesh@centos-barlesh.radidev.local>
2020-01-12 19:30:15 +0200

    es filters tests

---
+++ packages/db-controller/test/filters/es/es-filters.test.ts
@@ -0,0 +1,24 @@
+import "jest";
+
+describe("filter wrapper", () => {
+  test("create boolean filter - single", async () => {});
+
+  const request = {
+      where: [
+          {
+              key: "filter1",
+              type: "boolean",
+              value: true,
+              operator: "=",
+              path: ""
+
+          }
+      ]
+  }
+
+  const expected = {
+      
+  }
+
+
+});
